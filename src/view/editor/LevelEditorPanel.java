bc19930c5fa19b7999f73f99f4c23fd41caf04bc
saccolinux <93777997+vincenzosacco@users.noreply.github.com>
2025-06-30 19:08:49 +0200

    ADD-PANEL: LevelEditor
      add LevelEditorController and enhance LevelEditorPanel with sidebar and block buttons

--- src/view/editor/LevelEditorPanel.java
+++ src/view/editor/LevelEditorPanel.java
@@ -1,18 +1,47 @@
 package view.editor;
 
 import controller.ControllerObj;
+import model.game.GameConstants;
 import view.ViewComp;
+import view.game.BlocksImages;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class LevelEditorPanel extends JPanel implements ViewComp {
+    private final JPanel sidebar = new JPanel();
+
     public LevelEditorPanel(){
-        this.setBackground(Color.RED);
+
+       this.setBackground(Color.RED);
+        // Main layout
+        setLayout(new BorderLayout());
+
+        // Make components
+        makeSidebar();
+
+        // Add panels to frame
+        add(sidebar, BorderLayout.WEST);
+
     }
 
     @Override
     public void bindController(ControllerObj controller) {
 
+    }
+
+    // MAKE COMPONENTS //
+
+    private void makeSidebar() {
+        sidebar.setLayout(new BoxLayout(sidebar, BoxLayout.Y_AXIS));
+        sidebar.setBackground(Color.LIGHT_GRAY);
+
+        // ADD A BUTTON IMAGE FOR EACH BLOCK TYPE //
+
+        for (BufferedImage bImg: BlocksImages.getInstance().getAllDefaultImgs()) {
+            JButton button = new JButton(new ImageIcon(bImg));
+            sidebar.add(button);
+        }
     }
 }
